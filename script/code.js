ab662da1a13967dbbccc28115b52c49019fec836
amyj0hn <amyj62450@gmail.com>
2024-05-17 15:48:45 +0200

    added object in array

---
+++ script/code.js
@@ -0,0 +1,19 @@
+//Object
+let array = [
+    person = {
+    firstName: "Amy",
+    lastName: "Johnson",
+    age: 19,
+    female: true
+}]
+console.log(person)
+
+let array2 = [
+    person = {
+    firstName: "Jeremiah",
+    lastName: "Adriaanse",
+    age: 23,
+    female: false
+}]
+console.log(person)
+
